9bd0ec46f2283869c3363a3dd5fed950be45fab5
Adnan Siddiq <adnan.siddiq@live.com>
2023-10-30 18:47:05 +0500

    implement socket

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(process.env.APP_NAME)
   res.render('index', { title: 'Express' });
 });
+
+router.post('/send-sms', (req, res, next) => {
+
+  try {
+    const io = req.app.get('socketio');
+    const { to, message } = req.body
+
+    const payload = JSON.stringify({ to, message })
+
+    io.emit('send-sms', payload)
+
+    res.json({
+      message: "Sent"
+    })
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    })
+  }
+})
 
 module.exports = router;
